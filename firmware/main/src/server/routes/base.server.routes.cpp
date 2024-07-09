538a591ab3cd0fd359637ae17a1c407a50d8dc1d
Harrison Tyson <hty13@uclive.ac.nz>
2024-07-09 17:02:20 +1200

    Add missing alexa request check on NotFound endpoint

--- firmware/main/src/server/routes/base.server.routes.cpp
+++ firmware/main/src/server/routes/base.server.routes.cpp
@@ -11,7 +11,11 @@
 void base_routes(AsyncWebServer *server)
 {
     // Fallback route if not available
-    server->onNotFound(notFound);
+    server->onNotFound([](AsyncWebServerRequest *request)
+                       {
+        String body = (request->hasParam("body", true)) ? request->getParam("body", true)->value() : String();
+        if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), body)) return;
+        notFound(request); });
 
     // Set Wifi credentials
     server->on((String(BASE_ROUTE) + "/connect").c_str(), HTTP_POST, authorizeWifi);
