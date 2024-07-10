853a66d204c85448fb49f74717f33f2ee3763176
Harrison Tyson <hty13@uclive.ac.nz>
2024-07-10 14:57:07 +1200

    Add CRGB as entity

--- application/app/src/main/java/com/typhonh/infinitycube/model/entity/CRGB.kt
+++ application/app/src/main/java/com/typhonh/infinitycube/model/entity/CRGB.kt
@@ -1,14 +1,24 @@
 package com.typhonh.infinitycube.model.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class CRGB (
     @SerializedName("r")
+    @ColumnInfo
     val r: Int,
 
     @SerializedName("g")
+    @ColumnInfo
     val g: Int,
 
     @SerializedName("b")
-    val b: Int
+    @ColumnInfo
+    val b: Int,
+
+    @PrimaryKey
+    val crgbId: Long = 0,
 )
